56b68d1b77168f45e0c07f7ebaf838d118d8f6a6
shidenian <312856254@qq.com>
2025-02-27 15:31:22 +0800

    修改画笔效果ING

--- Assets/Scripts/WaterGame/Paint2DDynamicSize.cs
+++ Assets/Scripts/WaterGame/Paint2DDynamicSize.cs
@@ -1,41 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Paint2DDynamicSize : MonoBehaviour
 {
-    public LineRenderer lineRenderer;  // 通过Inspector关联LineRenderer
+    public Painting2d painting;
     public float MaxSize = 1.0f;
-    public float MinSize = 0.1f;
     public float rate = 5;
 
-    private float currentWidth;  // 当前的宽度
-
-    void Start()
+    public void Update()
     {
-        if (lineRenderer == null)
+
+        if (painting.isMouseDown)
         {
-            lineRenderer = GetComponent<LineRenderer>();  // 如果没有手动设置，自动查找LineRenderer组件
+            if (painting.widthPower < MaxSize)
+                painting.widthPower += Time.deltaTime * rate;
+        }
+        else
+        {
+
+            painting.widthPower = 0;
         }
 
-        currentWidth = lineRenderer.startWidth;  // 初始宽度
     }
 
-    void Update()
-    {
-        if (lineRenderer != null)
-        {
-            if (Input.GetMouseButton(0))  // 鼠标按下时，增加宽度
-            {
-                if (currentWidth < MaxSize)
-                    currentWidth += Time.deltaTime * rate;
-            }
-            else  // 鼠标松开时，逐渐减小宽度
-            {
-                currentWidth = Mathf.Clamp(currentWidth - Time.deltaTime * rate, MinSize, MaxSize);
-            }
 
-            // 更新LineRenderer的宽度
-            lineRenderer.startWidth = currentWidth;
-            lineRenderer.endWidth = currentWidth;
-        }
-    }
 }
---
+++ Assets/Scripts/WaterGame/Painting2DUtility.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painting2DUtility : MonoBehaviour
+{
+    public Painting2d painting;
+
+    public Texture[] burshStyles;
+
+
+    /// 设置画笔大小
+    public void SetPenSize(float v)
+    {
+        painting.widthPower = v;
+    }
+
+    /// 设置画笔样式
+    public void SetBurshStyle(int index)
+    {
+        index = index >= burshStyles.Length ? 0 : index;
+        painting.brushTypeTexture = burshStyles[index];
+    }
+
+    /// 设置画笔颜色
+    public void SetPenColor(Color color)
+    {
+        painting.brushColor = color;
+    }
+
+    /// 撤销
+    public void Cancel()
+    {
+        painting.CanclePaint();
+    }
+
+    /// 清空
+    public void Clear()
+    {
+        painting.OnClickClear();
+    }
+
+}
