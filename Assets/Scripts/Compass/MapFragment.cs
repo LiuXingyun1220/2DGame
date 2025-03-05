de6bbc4e1de683975a313b43449cfab5cc06392b
LAPTOP-JUE\jue <3071173847@qq.com>
2025-03-05 16:46:58 +0800

    Fragment

--- Assets/Scripts/Compass/MapFragment.cs
+++ Assets/Scripts/Compass/MapFragment.cs
@@ -1,13 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
-using System;
 using UnityEngine;
+using UnityEngine.UI;
 
-[Serializable]
-public class MapFragment
+public class MapFragment : MonoBehaviour
 {
-    public int fragmentID; // 地图碎片的唯一标识
-    public int requiredLevel; // 解锁该碎片所需的关卡编号
-    public Sprite fragmentSprite; // 地图碎片的图片
-    public bool isUnlocked; // 是否已解锁
+    public Image[] fragmentImages; // 地图碎片的Image数组
+    public Button[] fragmentButtons; // 地图碎片的Button数组
+    private bool[] isUnlocked; // 记录每个碎片是否解锁
+
+    void Start()
+    {
+        isUnlocked = new bool[fragmentImages.Length];
+        for (int i = 0; i < fragmentImages.Length; i++)
+        {
+            fragmentImages[i].color = new Color(0.5f, 0.5f, 0.5f, 1); // 设置暗淡颜色
+            fragmentButtons[i].interactable = false; // 禁用按钮
+            // 绑定点击事件
+            fragmentButtons[i].onClick.AddListener(() => OnFragmentClick(i));
+        }
+    }
+
+    // 解锁指定的碎片
+    public void UnlockFragment(int index)
+    {
+        if (index >= 0 && index < fragmentImages.Length)
+        {
+            fragmentImages[index].color = Color.white; // 设置为明亮状态
+            fragmentButtons[index].interactable = true; // 启用按钮
+            isUnlocked[index] = true; // 标记为解锁状态
+        }
+    }
+
+    // 碎片点击事件
+    public void OnFragmentClick(int index)
+    {
+        if (isUnlocked[index])
+        {
+            Debug.Log($"故事 {index} 被触发");
+            // 在这里添加讲述故事的逻辑
+        }
+    }
 }
