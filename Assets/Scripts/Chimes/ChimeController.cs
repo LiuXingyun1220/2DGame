c0390d909165ce728a1cd6c45ccf316b74fa40a6
LiuTing1220 <1075204097@qq.com>
2025-03-07 21:20:43 +0800

    wave effect

--- Assets/Scripts/Chimes/ChimeController.cs
+++ Assets/Scripts/Chimes/ChimeController.cs
@@ -6,29 +6,40 @@ using UnityEngine.UI;
 public class ChimeController : MonoBehaviour
 {
     public int chimeID;
-    public KeyCode triggerKey;
     private bool isActive;
 
     public GameObject chimeHighlight;
+    public ParticleSystem deactivateParticlePrefab;
 
     public void Activate()
     {
         isActive = true;
         chimeHighlight.SetActive(true);
-        Debug.Log(chimeID + " " + "亮了");
+        //Debug.Log(chimeID + " " + "亮了");
         StartCoroutine(AutoDeactivate());
     }
 
     private IEnumerator AutoDeactivate()
     {
-        yield return new WaitForSeconds(0.5f); // 判定窗口时间
+        yield return new WaitForSeconds(2f); // 判定窗口时间
         chimeHighlight.SetActive(false);
-        if (isActive) Debug.Log("错过了");
+        //if (isActive) Debug.Log("错过了");
     }
 
     public void Deactivate()
     {
+        // 生成粒子特效
+        if (deactivateParticlePrefab != null)
+        {
+            // 在风铃位置生成粒子，自动旋转匹配对象方向
+            Instantiate(deactivateParticlePrefab, transform.position, transform.rotation);
+
+            // 如果要附加到风铃对象上：
+            // Instantiate(deactivateParticlePrefab, transform.position, 
+            //     transform.rotation, transform);
+        }
         isActive = false;
-        Debug.Log("按下了");
+        chimeHighlight.SetActive(false);
+       
     }
 }
