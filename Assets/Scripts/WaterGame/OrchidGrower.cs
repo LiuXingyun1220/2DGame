8c0073f6076e58e5e5d3710c5e08395c22b7bf8e
shidenian <312856254@qq.com>
2025-05-27 15:57:26 +0800

    modify

--- Assets/Scripts/WaterGame/OrchidGrower.cs
+++ Assets/Scripts/WaterGame/OrchidGrower.cs
@@ -8,18 +8,21 @@ public class OrchidGrower : MonoBehaviour
     public Sprite youngSprite;
     public Sprite matureSprite;
     public Sprite bloomSprite;
+    public Sprite fullBloomSprite; // 第5种状态（完全绽放）
 
     void Update()
     {
         int score = Data.Score;
 
-        if (score < 2)
+        if (score < 1)
             spriteRenderer.sprite = sproutSprite;
-        else if (score < 3)
+        else if (score < 2)
             spriteRenderer.sprite = youngSprite;
-        else if (score < 4)
+        else if (score < 3)
             spriteRenderer.sprite = matureSprite;
-        else
+        else if (score < 4)
             spriteRenderer.sprite = bloomSprite;
+        else
+            spriteRenderer.sprite = fullBloomSprite;
     }
 }
